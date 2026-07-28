9fd423ecec97b187fe5c447f0a8ed9cb7bdff3ce
ravi <ravi2001pandey@gmail.com>
2026-07-29 01:31:14 +0530

    fix: force-track build/_redirects so Render serves SPA fallback on all routes

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ function App() {
         />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </>
   );
